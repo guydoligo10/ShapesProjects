3a58e3e159d3de7e6d2b93e0d5e484bdc97610be
guy <guydoligo10@gmail.com>
2026-07-29 00:35:35 +0300

    finished first assignment and built the painting

--- src/Main.java
+++ src/Main.java
@@ -3,48 +3,59 @@ public class Main {
 	//זה מחלקה ראשית לדוגמה כדי שתבינו מה בעצם אתם הולכים לראות, עליכם בעצם למחוק את מה שיש בפעולה הראשית עצמה, 
 	//לבנות במחלקה הראשית הזו את אוסף הצורות שלכם, ולבנות מחוץ לפעולה הראשית, אלא ממש כאן למטה
 	//את הפעולות הסטטיות שלכם
+    public static void move1(Square houseBody, Triangle roof, Rectangle door,
+                             Circle doorHandle, Square windowLeft, Square windowRight,
+                             Rectangle signBoard,Line windowLeftV, Line windowLeftH, Line windowRightV, Line windowRightH, Text signText, int newX, int newY) {
+        int currentX = 400;
+        int currentY = 250;
 
+        int deltaX = newX - currentX;
+        int deltaY = newY - currentY;
+
+        //הפינה השמאלית
+        int minXAfter = 400 + deltaX;
+        //הפינה הימנית
+        int maxXAfter = 600 + deltaX;
+        //הפינה העליונה שעל הגג
+        int minYAfter = 150 + deltaY;
+        //הפינה התחתונה
+        int maxYAfter = 450 + deltaY;
+
+
+        if (minXAfter < 0 || maxXAfter > 1000 || minYAfter < 0 || maxYAfter > 600) {
+            System.out.println("out of boundaries! try again with a different number...");
+            return;
+        }
+
+        houseBody.move(deltaX, deltaY);
+        roof.move(deltaX, deltaY);
+        door.move(deltaX, deltaY);
+        doorHandle.move(deltaX, deltaY);
+        windowLeft.move(deltaX, deltaY);
+        windowRight.move(deltaX, deltaY);
+        signBoard.move(deltaX, deltaY);
+        windowLeftV.move(deltaX, deltaY);
+        windowLeftH.move(deltaX, deltaY);
+        windowRightV.move(deltaX, deltaY);
+        windowRightH.move(deltaX, deltaY);
+        signText.move(deltaX, deltaY);
+    }
 	public static void main(String[] args) {
-        int placeX = 100;
-        int placeY = 100;
-        int size = 10;
 
-        Square s1 = new Square(placeX, placeY, size, "red");
+                Square houseBody = new Square(400, 250, 200, "red");
+                Triangle roof = new Triangle(500, 150, 200, 100, "yellow");
+                Rectangle door = new Rectangle(475, 330, 50, 120, "blue");
+                Circle doorHandle = new Circle(515, 390, 5, "white");
+                Square windowLeft = new Square(420, 280, 40, "blue");
+                Square windowRight = new Square(540, 280, 40, "blue");
+                Line windowLeftV = new Line(440, 280, 440, 320, "black");
+                Line windowLeftH = new Line(420, 300, 460, 300, "black");
+                Line windowRightV = new Line(560, 280, 560, 320, "black");
+                Line windowRightH = new Line(540, 300, 580, 300, "black");
+                Rectangle signBoard = new Rectangle(415, 350, 50, 30, "magenta");
+                Text signText = new Text(420, 370, "HOME", "black");
 
-        for (int i = 0; i < 10; i++)
-                s1.move(size, size);
+        move1(houseBody, roof, door, doorHandle, windowLeft, windowRight, signBoard, windowLeftV, windowLeftH, windowRightV, windowRightH, signText, 500, 200);
 
-        placeX = 400;
-        placeY = 200;
-        Circle c1 = new Circle(placeX, placeY, size * 4, "red");
-        Circle c2 = new Circle(placeX, placeY, size * 3, "yellow");
-        Circle c3 = new Circle(placeX, placeY, size * 2, "green");
-        Circle c4 = new Circle(placeX, placeY, size, "blue");
-
-        Text name = new Text(600, 300, "Yod OLE!", "magenta");
-
-        placeX = 100;
-        placeY = 400;
-        size = 200;
-
-        Triangle t2 = new Triangle(placeX, placeY, size, 140, "red");
-        Triangle t3 = new Triangle(placeX, placeY, size, 140, "green");
-        Triangle t4 = new Triangle(placeX, placeY, size, 140, "cyan");
-
-        t2.move(size, 0);
-        t3.move(size * 2, 0);
-        t4.move(size * 3, 0);
-        Triangle t1 = new Triangle(placeX, placeY, size, 140, "cyan");
-        Circle m1 = new Circle(50, 50, 30, "red");
-        placeX = 40;
-        placeY = 35;
-        size = 5;
-        Circle leftEye = new Circle(placeX, placeY, size, "white");
-        Circle rightEye = new Circle(placeX + 15, placeY, size, "white");
-        Circle hair1 = new Circle(placeX - 10, placeY - 10, size, "green");
-        Circle hair2 = new Circle(placeX - 10, placeY - 10, size, "green");
-
-    
-}
-
-}
+    }
+    }
